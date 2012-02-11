c2581a3b1a9c6e2cd04648877124a146cabdd0e2
unknown <jsc@Hanuman.(none)>
2012-02-11 00:06:45 -0700

    Added commands to and merged renderers into a single class

--- PandocRender.py
+++ PandocRender.py
@@ -49,3 +49,40 @@ class PandocRenderCommand(sublime_plugin.TextCommand):
 
         if openInBrowser:
             webbrowser.open_new_tab(tmp_html.name)
+
+
+class PandocRenderDocxCommand(sublime_plugin.TextCommand):
+    """ render file contents to HTML and, optionally, open in your web browser"""
+
+    def getTemplatePath(self):
+        filename = 'reference.docx'
+        path = os.path.join(sublime.packages_path(), 'SublimePandoc', filename)
+        if not os.path.isfile(path):
+            raise Exception(filename + " file not found!")
+        return path
+
+    def run(self, edit):
+        region = sublime.Region(0, self.view.size())
+        encoding = self.view.encoding()
+        if encoding == 'Undefined':
+            encoding = 'UTF-8'
+        elif encoding == 'Western (Windows 1252)':
+            encoding = 'windows-1252'
+        contents = self.view.substr(region)
+
+        tmp_md = tempfile.NamedTemporaryFile(delete=False, suffix=".md")
+        tmp_md.write(contents)
+        tmp_md.close()
+
+        output_filename = os.path.splitext(self.view.file_name())[0]+".docx"
+
+        cmd = 'pandoc -t docx --reference-docx="%s" "%s" -o "%s"' % (
+            self.getTemplatePath(),
+            tmp_md.name,           
+            output_filename)
+        if '[[TOC]]' in contents:
+            cmd += " --toc"
+        if '[[NUM]]' in contents:
+            cmd += " -N"
+
+        os.system(cmd)
