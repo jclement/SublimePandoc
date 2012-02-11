f43400c3560d80cc92756fdef583367af8aa5ad1
unknown <jsc@e276-jsc.eni.local>
2012-02-10 22:56:05 -0700

    Initial

---
+++ MarkdownRender.py
@@ -0,0 +1,51 @@
+import sublime, sublime_plugin
+import webbrowser
+import tempfile
+import os
+
+
+class MarkdownRenderCommand(sublime_plugin.TextCommand):
+    """ render file contents to HTML and, optionally, open in your web browser"""
+
+    def getTemplatePath(self):
+        filename = 'template.html'
+        # path via package manager
+        path = os.path.join(sublime.packages_path(), 'JSC.Markdown', filename)
+        if not os.path.isfile(path):
+            raise Exception(filename + " file not found!")
+        return path
+
+    def run(self, edit, openInBrowser=True, writeBeside=False):
+        print edit, openInBrowser, self.getTemplatePath()
+        region = sublime.Region(0, self.view.size())
+        encoding = self.view.encoding()
+        if encoding == 'Undefined':
+            encoding = 'UTF-8'
+        elif encoding == 'Western (Windows 1252)':
+            encoding = 'windows-1252'
+        contents = self.view.substr(region)
+
+        tmp_md = tempfile.NamedTemporaryFile(delete=False, suffix=".md")
+        tmp_md.write(contents)
+        tmp_md.close()
+
+        if writeBeside:
+            output_filename = os.path.splitext(self.view.file_name())[0]+".html"
+        else:
+            tmp_html = tempfile.NamedTemporaryFile(delete=False, suffix=".html")
+            tmp_html.close()
+            output_filename=tmp_html.name
+
+        cmd = 'pandoc -t html5 --standalone --template="%s" "%s" -o "%s"' % (
+            self.getTemplatePath(),
+            tmp_md.name,           
+            output_filename)
+        if '[[TOC]]' in contents:
+            cmd += " --toc"
+        if '[[NUM]]' in contents:
+            cmd += " -N"
+
+        os.system(cmd)
+
+        if openInBrowser:
+            webbrowser.open_new_tab(tmp_html.name)
